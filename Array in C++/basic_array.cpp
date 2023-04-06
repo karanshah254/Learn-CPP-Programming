dcc63c06064c9ac602a3b3e73594130c79639781
Karan Shah <karankshah2504@gmail.com>
2023-04-06 23:17:27 +0530

    Started Array in C++

---
+++ Array in C++/basic_array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num[5] = {1, 2, 3, 4, 5};
+    int i;
+
+    cout << "Done With Array Initialization"
+         << endl
+         << "Thank You";
+
+    return 0;
+}
---
+++ Array in C++/print_value.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number[10] = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+
+    cout << "Welcome To The Array";
+
+    cout << "Printing the user required numbers";
+
+    cout << endl;
+
+    cout << endl
+         << number[5]
+         << endl
+         << number[6]
+         << endl
+         << number[9];
+
+    return 0;
+}
