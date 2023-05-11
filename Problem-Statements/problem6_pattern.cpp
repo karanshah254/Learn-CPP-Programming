5284426cceb509f220af51fa6ae9424c7cb93567
Karan Shah <karankshah2504@gmail.com>
2023-05-12 00:53:18 +0530

    Learn Patterns

--- Problem-Statements/problem6_pattern.cpp
+++ Problem-Statements/problem6_pattern.cpp
@@ -1,7 +1,34 @@
+
+/*
+
+*
+**
+***
+****
+*****
+
+*/
+
 #include <iostream>
-using namespace std ;
+using namespace std;
 
 int main()
 {
-    
+    int i, j;
+    int n;
+
+    cout << "Enter Rows : ";
+    cin >> n;
+
+    cout << "\n";
+    for (int i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    cout << "\n";
 }
---
+++ Problem-Statements/short_cut_pattern.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// Right Angle traingle .
+
+int main()
+{
+    cout << "Welcome to pattern problems...\n\n";
+
+    cout << "*\n";
+    cout << "**\n";
+    cout << "***\n";
+}
